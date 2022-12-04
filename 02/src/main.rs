28f90b970d5f17ddb5bfe7fcef7dabdd2eaccd47
Daniel Leech <dan.t.leech@gmail.com>
2022-12-04 20:28:41 +0100

    Day 2

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -25,6 +25,27 @@ impl Weapon {
             Weapon::Rock => 1,
         }
     }
+
+    fn to_achieve_result(&self, er: &Result) -> Weapon {
+        match er {
+            Result::Won => match self {
+                Weapon::Paper => Weapon::Scissors,
+                Weapon::Scissors => Weapon::Rock,
+                Weapon::Rock => Weapon::Paper,
+            },
+            Result::Draw => match self {
+                Weapon::Paper => Weapon::Paper,
+                Weapon::Scissors => Weapon::Scissors,
+                Weapon::Rock => Weapon::Rock,
+            } ,
+            Result::Lost =>match self {
+                Weapon::Paper => Weapon::Rock,
+                Weapon::Scissors => Weapon::Paper,
+                Weapon::Rock => Weapon::Scissors,
+            },
+        }
+        
+    }
 }
 impl Weapon {
     fn fights(&self, s2: &Weapon) -> Result {
@@ -53,13 +74,18 @@ fn weapons_map() -> HashMap<&'static str, Weapon> {
         ("A", Weapon::Rock),
         ("B", Weapon::Paper),
         ("C", Weapon::Scissors),
-
-        ("X", Weapon::Rock),
-        ("Y", Weapon::Paper),
-        ("Z", Weapon::Scissors),
     ].iter().cloned().collect()
 }
 
+fn results_map() -> HashMap<&'static str, Result> {
+    vec![
+        ("X", Result::Lost),
+        ("Y", Result::Draw),
+        ("Z", Result::Won),
+    ].iter().cloned().collect()
+}
+
+#[derive(Clone)]
 enum Result {
     Won,
     Draw,
@@ -78,10 +104,12 @@ impl Result {
 
 pub fn calculate(pairs: Vec<(&str, &str)>) -> i32 {
     let weapons = weapons_map();
+    let results_map = results_map();
 
     let rounds = pairs.iter().map(|(p1, p2)| -> i32 {
         let w1 = weapons.get(p1).unwrap();
-        let w2 = weapons.get(p2).unwrap();
+        let er = results_map.get(p2).unwrap();
+        let w2 = w1.to_achieve_result(er);
         let round = w2.fights(w1);
 
         round.score() + w2.value()
