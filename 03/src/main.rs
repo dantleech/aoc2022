c852fec99c59cd970decaa7eecac67a425ffdd11
Daniel Leech <dan.t.leech@gmail.com>
2022-12-06 22:31:01 +0100

    Part 1

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -2,28 +2,58 @@ use std::{collections::HashMap, fs};
 
 fn main() {
     let input: String = fs::read_to_string("data/input").expect("nope");
-    calculate(input);
+    println!("{}", calculate(input));
 }
+
+type Backpack = (Vec<char>,Vec<char>);
+type Backpacks = Vec<Backpack>;
 
 pub fn calculate(input: String) -> i32 {
 
-    let backpacks: Vec<(Vec<char>,Vec<char>)> = input.trim().split("\n").into_iter().map(|line| -> (Vec<char>,Vec<char>) {
-        let compartment1 = &line[0..line.len() / 2];
-        let compartment2 = &line[line.len() / 2..line.len()];
-        (compartment1.trim().chars().collect(),compartment2.trim().chars().collect())
+    let backpacks = create_backpacks(input);
+    let common_items = common_items(backpacks);
+    let priority_map = priority_map();
 
+    let cips: Vec<i32> = common_items.iter().map(|i: &char| -> i32 {
+        *priority_map.get(&i).unwrap()
     }).collect();
+    cips.iter().sum()
+}
 
+pub fn create_backpacks(input: String) -> Backpacks {
+   input.trim().split("\n").into_iter().map(|line| -> Backpack {
+       let line = line.trim();
+        let c1 = &line[0..line.len() / 2];
+        let c2 = &line[line.len() / 2..line.len()];
+        (
+            c1.chars().collect(),
+            c2.chars().collect()
+        )
+    }).collect()
+}
 
-    12
+fn common_items(backpacks: Backpacks) -> Vec<char> {
+    backpacks.into_iter().map(|b| -> char {
+        let c1 = &b.0;
+        let c2 = &b.1;
+
+        for i1 in c1 {
+            for i2 in c2 {
+                if i1 == i2 {
+                    return *i1;
+                }
+            }
+        }
+        panic!("Could not determine common item");
+    }).collect()
 }
 
 fn priority_map() -> HashMap<char, i32> {
     let letters = vec![
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-        'o', 'p', 'q', 'r', 's', 't', 'u', 'z', 'w', 'x', 'y', 'z',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
-        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'Z', 'W', 'X', 'Y', 'Z',
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     ];
     let mut map: HashMap<char,i32> = HashMap::new();
     let mut i = 1;
@@ -40,7 +70,7 @@ mod test {
 
     #[test]
     fn calculates_score_based_on_guide() {
-        assert_eq!(15, calculate("
+        assert_eq!(157, calculate("
             vJrwpWtwJgWrhcsFMMfFFhFp
             jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
             PmmdzqPrVvPwwTWBwg
