723369ec55fe157fccf864c26026330454ca8291
Daniel Leech <dan.t.leech@gmail.com>
2022-12-11 13:27:44 +0100

    Parse te input

--- 04/main.go
+++ 04/main.go
@@ -1,18 +1,71 @@
 package main
 
-import "io/ioutil"
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/samber/lo"
+)
+
+type SectorRange struct {
+    start int;
+    end int;
+}
+
+type Pair struct {
+    range1 SectorRange;
+    range2 SectorRange;
+}
 
 func numberOfOverlappingPairs(filename string) int {
-    readInput(filename)
+    spew.Dump(sectorRanges(filename))
     return 0
 }
 
+func sectorRanges(filename  string) []Pair {
+    return lo.Map(
+        strings.Split(readInput(filename), "\n"), 
+        func (line string, index int) Pair {
+
+            return (func (ranges []SectorRange) Pair {
+                return Pair{
+                    range1: ranges[0],
+                    range2: ranges[1],
+                }
+            })(lo.Map(
+                lo.Map(
+                    strings.Split(line, ","),
+                    func (stringRange string, _ int) []string {
+                        return strings.Split(stringRange, "-")
+                    },
+                ),
+                func (stringRanges []string, _ int) SectorRange {
+                    return SectorRange{
+                        start: strToInt(stringRanges[0]),
+                        end: strToInt(stringRanges[1]),
+                    }
+                },
+            ))
+        },
+    )
+}
+
+func strToInt(string string) int {
+    int, err := strconv.Atoi(string)
+    if err != nil {
+        panic(err)
+    }
+    return int
+}
+
 func readInput(filename string) string {
-	data, err := ioutil.ReadFile(filename)
+    data, err := ioutil.ReadFile(filename)
 
-	if err != nil {
-		panic(err)
-	}
+    if err != nil {
+        panic(err)
+    }
 
-    return string(data)
+    return strings.TrimSpace(string(data))
 }
